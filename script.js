423e597972f8f008c3b7f4b7146437538c252dac
pamartinezp <42811622+pamartinezp@users.noreply.github.com>
2018-08-29 22:03:05 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+var vect=[];
+vect.push([",--3--","---","---","---<br>"]);
+vect.push(["--2--","---","---","---<br>"]);
+vect.push(["--1--","---","---","---<br>"]);
+vect.push(["-----","-1-","-2-","-3-<br>"]);
 window.onload = function()
 {
     console.log("hola mundo");
@@ -5,16 +10,96 @@ window.onload = function()
 function generar()
 {
     console.log("holamundo");
-    var i,j;
-    for(i=0; i<=2; i++)
+    salida.innerHTML=vect;
+}
+function jugar()
+{
+    console.log("entro a funcion jugar")
+    var dx=document.getElementById("cx");
+    var cx=dx.value;
+    var dy=document.getElementById("cy");
+    var cy=dy.value;
+    console.log("las coordenadas son: "+cx+" en x y "+cy+" en y ");
+    if (cx==1 && cy==1)
     {
-        console.log(i);
-        salida.innerHTML=salida.innerHTML+i;
-        for (j=0; j<=2; j++) 
+        console.log("coordenadas: "+cx+" y "+cy);
+        vect[2][1]="-x-";
+        salida.innerHTML=vect;
+    } 
+    else 
+    {
+        if (cx==1 && cy==2)
         {
-            console.log(j);
-            salida.innerHTML=salida.innerHTML+j;  
-        }
-        salida.innerHTML=salida.innerHTML+"<br>";
+            console.log("coordenadas: "+cx+" y "+cy);
+            vect[2][2]="-x-";
+            salida.innerHTML=vect;
+        } 
+        else 
+        {
+            if (cx==1 && cy==3)
+            {
+                console.log("coordenadas: "+cx+" y "+cy);
+                vect[2][3]="-x-<br>";
+                salida.innerHTML=vect;
+            } 
+            else 
+            {
+                if (cx==2 && cy==1)
+                {
+                    console.log("coordenadas: "+cx+" y "+cy);
+                    vect[1][1]="-x-";
+                    salida.innerHTML=vect;
+                } 
+                else 
+                {
+                    if (cx==2 && cy==2)
+                    {
+                        console.log("coordenadas: "+cx+" y "+cy);
+                        vect[1][2]="-x-";
+                        salida.innerHTML=vect;
+                    } 
+                    else 
+                    {
+                        if (cx==2 && cy==3)
+                        {
+                            console.log("coordenadas: "+cx+" y "+cy);
+                            vect[1][3]="-x-<br>";
+                            salida.innerHTML=vect;
+                        } 
+                        else 
+                        {
+                            if (cx==3 && cy==1)
+                            {
+                                console.log("coordenadas: "+cx+" y "+cy);
+                                vect[0][1]="-x-";
+                                salida.innerHTML=vect;
+                            } 
+                            else 
+                            {
+                                if (cx==3 && cy==2)
+                                {
+                                    console.log("coordenadas: "+cx+" y "+cy);
+                                    vect[0][2]="-x-";
+                                    salida.innerHTML=vect;
+                                } 
+                                else 
+                                {
+                                    if (cx==3 && cy==3)
+                                    {
+                                        console.log("coordenadas: "+cx+" y "+cy);
+                                        vect[0][3]="-x-<br>";
+                                        salida.innerHTML=vect;
+                                    } 
+                                    else 
+                                    {
+                                        console.log("coordenadas erroneas");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                } 
+            }
+        }     
     }
 }
