7ada6a385e2b4c9470b6b2dadd01532d6ed2d71b
pamartinezp <42811622+pamartinezp@users.noreply.github.com>
2018-09-03 12:26:18 -0500

    Add files via upload
    cuarto commit del triki

--- script.js
+++ script.js
@@ -1,211 +1,120 @@
-var vect=[];
-vect.push([",--3--","---","---","---<br>"]);
-vect.push(["--2--","---","---","---<br>"]);
-vect.push(["--1--","---","---","---<br>"]);
-vect.push(["-----","-1-","-2-","-3-<br>"]);
-var tm1=0;
-var tm2=0;
-var tm3=0;
-var tm4=0;
-var tm5=0;
-var tm6=0;
-var tm7=0;
-var tm8=0;
-var tm9=0;
+//inicio codigo
+// creacion de la matriz:
+
+var mat=[];
+mat.push([1,1,1]);
+mat.push([1,1,1]);
+mat.push([1,1,1]);
+//funcion windows on load
+
 window.onload = function()
 {
-    console.log("hola mundo");
+    console.log("funciono");
 }
+
 function generar()
 {
     console.log("holamundo");
-    salida.innerHTML=vect;
+    salida.innerHTML=mat[0]+"<br>";
+    salida.innerHTML+=mat[1]+"<br>";
+    salida.innerHTML+=mat[2]+"<br>";
 }
+
+function ganah(filcol,des)
+{
+    console.log("--entro a funcion gana--");
+    var txt="";
+    //ganador horizontal
+    for(var i=0; i<=2; i++)
+    {
+        console.log("--entro a for--");
+        if (des==1) 
+        {
+            txt+=mat[filcol][i]; 
+        } 
+        else if(des==2)
+        {
+            txt+=mat[i][filcol];
+        }
+        else if(des==3)
+        {
+            txt+=mat[i][i];
+        }
+        else 
+        {
+            txt+=mat[i][2-i];
+            console.log(txt);
+        }
+    }
+    console.log("--salio del for--");
+    return txt;
+}
+
+//****************************************************funcion jugar**********************************************************
 function jugar()
 {
-    console.log("entro a funcion jugar")
+    //entro a la funcion.....
+    console.log("entro a funcion jugar");
+
+    //obtencion de las variables
     var dx=document.getElementById("cx");
     var cx=dx.value;
     var dy=document.getElementById("cy");
     var cy=dy.value;
     console.log("las coordenadas son: "+cx+" en x y "+cy+" en y ");
-    if (cx==1 && cy==1)
+
+    //casillas ya marcadas....
+    if (mat[cx-1][cy-1]==1) 
     {
-        console.log(tm1);
-        if (tm1==0) 
+        alert("casilla no marcada");
+        mat[cx-1][cy-1]="x";
+        generar();
+        //*************************************************juega la maquina***********************************************/
+        alert("juega la maquina");
+        var iw=0;
+        var wb=0;
+        while(iw==0)
         {
-            console.log("coordenadas: "+cx+" y "+cy);
-            vect[2][1]="-x-";
-            tm1=1;
-            salida.innerHTML=vect;   
-        } 
-        else 
-        {
-            alert("casilla ya marcada");   
+            var numalex=Math.floor((Math.random()*3)+1);
+            console.log("---numero aleatorio en x ---: ",numalex);
+            var numaley=Math.floor((Math.random()*3)+1);
+            console.log("---numero aleatorio en y ---: ",numaley);
+            if (mat[numalex-1][numaley-1]==1) 
+            {
+                mat[numalex-1][numaley-1]="o";
+                iw=1;
+                generar();
+            } 
+            else 
+            {
+                wb=wb+1;
+                if (wb==20) 
+                {
+                    iw=1;
+                    alert("todas las casillas han sido marcadas ya");
+                }
+            }
         }
     } 
     else 
     {
-        if (cx==1 && cy==2)
-        {
-            console.log(tm2);
-            if (tm2==0) 
-            {
-                console.log("coordenadas: "+cx+" y "+cy);
-                vect[2][2]="-x-";
-                tm2=1;
-                salida.innerHTML=vect;   
-            } 
-            else 
-            {
-                alert("casilla ya marcada");   
-            }
-        } 
-        else 
-        {
-            if (cx==1 && cy==3)
-            {  
-                console.log(tm2);
-                if (tm3==0) 
-                {
-                    console.log("coordenadas: "+cx+" y "+cy);
-                    vect[2][3]="-x-<br>";
-                    tm3=1;
-                    salida.innerHTML=vect;   
-                } 
-                else 
-                {
-                    alert("casilla ya marcada");   
-                }
-            } 
-            else 
-            {
-                if (cx==2 && cy==1)
-                {
-                    console.log(tm4);
-                    if (tm4==0) 
-                    {
-                        console.log("coordenadas: "+cx+" y "+cy);
-                        vect[1][1]="-x-";
-                        tm4=1;
-                        salida.innerHTML=vect;   
-                    } 
-                    else 
-                    {
-                        alert("casilla ya marcada");   
-                    }
-                } 
-                else 
-                {
-                    if (cx==2 && cy==2)
-                    {
-                        /*console.log("coordenadas: "+cx+" y "+cy);
-                        vect[1][2]="-x-";
-                        salida.innerHTML=vect;*/
-                        console.log(tm5);
-                        if (tm5==0) 
-                        {
-                            console.log("coordenadas: "+cx+" y "+cy);
-                            vect[1][2]="-x-";
-                            tm5=1;
-                            salida.innerHTML=vect;   
-                        } 
-                        else 
-                        {
-                            alert("casilla ya marcada");   
-                        }
-                    } 
-                    else 
-                    {
-                        if (cx==2 && cy==3)
-                        {
-                            /*console.log("coordenadas: "+cx+" y "+cy);
-                            vect[1][3]="-x-<br>";
-                            salida.innerHTML=vect;*/
-                            console.log(tm6);
-                            if (tm6==0) 
-                            {
-                                console.log("coordenadas: "+cx+" y "+cy);
-                                vect[1][3]="-x-<br>";
-                                tm6=1;
-                                salida.innerHTML=vect;   
-                            } 
-                            else 
-                            {
-                                alert("casilla ya marcada");   
-                            }
-                        } 
-                        else 
-                        {
-                            if (cx==3 && cy==1)
-                            {
-                                /*console.log("coordenadas: "+cx+" y "+cy);
-                                vect[0][1]="-x-";
-                                salida.innerHTML=vect;*/
-                                console.log(tm7);
-                                if (tm7==0) 
-                                {
-                                    console.log("coordenadas: "+cx+" y "+cy);
-                                    vect[0][1]="-x-";
-                                    tm7=1;
-                                    salida.innerHTML=vect;   
-                                } 
-                                else 
-                                {
-                                    alert("casilla ya marcada");   
-                                }
-                            }
-                            else 
-                            {
-                                if (cx==3 && cy==2)
-                                {
-                                    /*console.log("coordenadas: "+cx+" y "+cy);
-                                    vect[0][2]="-x-";
-                                    salida.innerHTML=vect;*/
-                                    console.log(tm8);
-                                    if (tm8==0) 
-                                    {
-                                        console.log("coordenadas: "+cx+" y "+cy);
-                                        vect[0][2]="-x-";
-                                        tm8=1;
-                                        salida.innerHTML=vect;   
-                                    } 
-                                    else 
-                                    {
-                                        alert("casilla ya marcada");   
-                                    }
-                                } 
-                                else 
-                                {
-                                    if (cx==3 && cy==3)
-                                    {
-
-                                        /*console.log("coordenadas: "+cx+" y "+cy);
-                                        vect[0][3]="-x-<br>";
-                                        salida.innerHTML=vect;*/
-                                        console.log(tm9);
-                                        if (tm9==0) 
-                                        {
-                                            console.log("coordenadas: "+cx+" y "+cy);
-                                            vect[0][3]="-x-<br>";
-                                            tm9=1;
-                                            salida.innerHTML=vect;   
-                                        } 
-                                        else 
-                                        {
-                                        alert("casilla ya marcada");   
-                                        }
-                                    } 
-                                    else 
-                                    {
-                                        console.log("coordenadas erroneas");
-                                    }
-                                }
-                            }
-                        }
-                    }
-                } 
-            }
-        }     
+        alert("casilla ya marcada");    
     }
+    //******************************************************desicion de quien ganó******************************************************
+    for(var k=1; k<=4; k++)
+    {
+        for(var i=0; i<=2; i++)
+        {
+            console.log("---antes gana---");
+            console.log("estado del vector: "+ganah(i,k));
+            var htxt=ganah(i,k);
+            console.log("---despues gana---")
+            if(htxt=="xxx")
+            {
+                alert("gano el usuario");
+                break;
+            }
+        }
+    }
+    
 }
